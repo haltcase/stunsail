65bb0189867255a7fcb4a288ec80e28d7e4caef0
Bo Lingen <thecitycide@gmail.com>
2017-01-31 15:23:51 -0600

    fix(tests): throws & notThrows usage

--- tests/util/invariant.js
+++ tests/util/invariant.js
@@ -2,19 +2,19 @@ import test from 'ava'
 import fn from '../../util/invariant'
 
 test('throws on falsy conditions', t => {
-  let error = t.throws(fn(false))
+  let error = t.throws(() => fn(false))
 
   t.is(error.name, 'InvariantError')
   t.is(error.message, 'Invariant Violation')
 })
 
 test('uses provided custom error message', t => {
-  let error = t.throws(fn(false, 'Ya dun goofed'))
+  let error = t.throws(() => fn(false, 'Ya dun goofed'))
 
   t.is(error.name, 'InvariantError')
   t.is(error.message, 'Ya dun goofed')
 })
 
 test('does not throw on truthy conditions', t => {
-  t.notThrows(fn(true))
+  t.notThrows(() => fn(true))
 })
