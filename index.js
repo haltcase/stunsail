0609ed562956ba5b41e20003aea1aa16db1775ca
citycide <thecitycide@gmail.com>
2017-02-24 14:46:06 -0600

    fix: add `col` collection to main exports

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 exports.is = require('./is')
 exports.to = require('./to')
 exports.fn = require('./fn')
+exports.col = require('./col')
 exports.util = require('./util')
 exports.async = require('./async')
 exports.getType = require('./get-type')
