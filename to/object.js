7d59c10613d022d2d2bdf0c45aeb8fadccfec235
citycide <thecitycide@gmail.com>
2017-02-25 19:47:25 -0600

    feat(to.object): handle a few more input types

--- to/object.js
+++ to/object.js
@@ -1,9 +1,16 @@
 'use strict'
 
-module.exports = array => {
-  if (!Array.isArray(array)) return {}
+const getType = require('../get-type')
+const isPrimitive = require('../is/primitive')
 
-  return array.reduce((acc, key) => {
+module.exports = value => {
+  if (isPrimitive(value)) return { [value]: value }
+
+  let inputType = getType(value)
+  if (inputType === 'object') return value
+  if (inputType !== 'array') return {}
+
+  return value.reduce((acc, key) => {
     if (Array.isArray(key) && key.length === 2) {
       return Object.assign(acc, { [key[0]]: key[1] })
     }
