336a94a678eb03efc44824021ed68b48d27e3bf0
Bo Lingen <thecitycide@gmail.com>
2017-03-03 14:25:39 -0600

    feat(col): add `match()` method (#9)
    * feat(col): add `match()` method
    
    Add `match()` for object property comparison. The curried version is useful as the predicate for native Array methods like `find()`.
    
    * fix(lint): remove trailing spaces
    
    * test(col): add `match()` tests
    
    * fix: missing quotes on strings

---
+++ col/match.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const each = require('./each')
+const isEqual = require('../is/equal')
+const curry2 = require('../fn/curry2')
+
+module.exports = curry2((compare, object) => {
+  let match = true
+  each((value, key) => {
+    if (!isEqual(value, object[key])) {
+      match = false
+      return false
+    }
+  }, compare)
+
+  return match
+})
