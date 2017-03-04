cda6ab0512fbabcedc012f51af97633ae2295ec1
Bo Lingen <thecitycide@gmail.com>
2017-03-04 06:02:34 +0000

    test(is/to): add remaining tests

---
+++ tests/col/has.js
@@ -0,0 +1,13 @@
+import test from 'ava'
+import fn from '../../col/has'
+
+const object = { key: 'value', nested: { inner: { deep: 'thing' } } }
+
+test(`returns true/false based on path's existence in the object`, t => {
+  t.true(fn('nested.inner.deep', object))
+  t.false(fn('super.deep.ultra.nested.inner.path', object))
+})
+
+test('auto-curried at two arguments', t => {
+  t.is(typeof fn('key'), 'function')
+})
