c5521e0428cc643151ef12976000780401a2a310
Bo Lingen <thecitycide@gmail.com>
2017-03-10 00:19:35 +0000

    fix(col.match): fix name collision

--- col/match.js
+++ col/match.js
@@ -5,13 +5,13 @@ const isEqual = require('../is/equal')
 const curry = require('../fn/curry')
 
 module.exports = curry(function match (compare, object) {
-  let match = true
+  let isMatch = true
   each((value, key) => {
     if (!isEqual(value, object[key])) {
-      match = false
+      isMatch = false
       return false
     }
   }, compare)
 
-  return match
+  return isMatch
 })
