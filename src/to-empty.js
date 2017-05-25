44ec0cf89c7f0bbb91617773d777f55318d2b023
Bo Lingen <thecitycide@gmail.com>
2017-05-25 22:16:17 +0000

    feat: add to-empty
    Add `toEmpty()` for creating stub objects of various types.

---
+++ src/to-empty.js
@@ -0,0 +1,23 @@
+import getType from './get-type'
+
+function toEmpty (type) {
+  let inputType = getType(type)
+
+  if (inputType !== 'string') {
+    return toEmpty(inputType)
+  }
+
+  switch (type) {
+    case 'array': return []
+    case 'object': return {}
+    case 'string': return ''
+    case 'number': return 0
+    case 'boolean': return false
+    case 'set': return new Set()
+    case 'map': return new Map()
+    case 'null': return null
+    case 'undefined': return undefined
+  }
+}
+
+export default toEmpty
---
+++ tests/to-empty.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import fn from '../to-empty'
+
+test('returns an empty object of the provided type', t => {
+  t.is(fn('string'), '')
+  t.is(fn('number'), 0)
+  t.is(fn('boolean'), false)
+  t.is(fn('null'), null)
+  t.is(fn('undefined'), undefined)
+  t.deepEqual(fn('object'), {})
+  t.deepEqual(fn('array'), [])
+  t.deepEqual(fn('set'), new Set())
+  t.deepEqual(fn('map'), new Map())
+})
+
+test('returns matching type if not provided string', t => {
+  t.deepEqual(fn(null), null)
+  t.deepEqual(fn(undefined), undefined)
+  t.deepEqual(fn(1000), 0)
+  t.deepEqual(fn([1, 2, 3, 4]), [])
+  t.deepEqual(fn({ key: 'value' }), {})
+})
