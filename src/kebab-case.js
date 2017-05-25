9cdaa54ead8a380c144477305f864cc375353f91
Bo Lingen <thecitycide@gmail.com>
2017-05-25 22:05:45 +0000

    feat: add kebab-case

---
+++ src/kebab-case.js
@@ -0,0 +1,12 @@
+import {
+  WORD_SEPARATOR_REGEX as separators,
+  UPPERCASE_CHARACTER_REGEX as uppers
+} from './constants'
+
+export default string => {
+  return string
+    .replace(uppers, match => ' ' + (match.toLowerCase() || match))
+    .trim()
+    .split(separators)
+    .join('-')
+}
