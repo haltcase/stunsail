351543253d32b9e4f8bf001d5e9ad120f7598ffc
Bo Lingen <thecitycide@gmail.com>
2017-05-25 22:40:03 +0000

    feat: add kebab-case

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,5 @@ export const MAX_SAFE_INTEGER = 9007199254740991
 export const OCTAL_REGEX = /^0o[0-7]+$/i
 export const BAD_HEX_REGEX = /^[-+]0x[0-9a-f]+$/i
 export const BINARY_REGEX = /^0b[01]+$/i
+export const WORD_SEPARATOR_REGEX = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-./:;<=>?@[\]^_`{|}~]+/
+export const UPPERCASE_CHARACTER_REGEX = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]/g
