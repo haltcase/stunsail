658aefc699d5136643e384c3cee69a6a1857d38f
Bo Lingen <thecitycide@gmail.com>
2017-06-16 18:29:09 +0000

    feat(is-one-of): support `Map` and `Set`

--- tests/is-one-of.js
+++ tests/is-one-of.js
@@ -4,6 +4,8 @@ import fn from '../is-one-of'
 const str = 'string'
 const arr = [1, 2, 3, 4, 5]
 const obj = { name: 'shady', genre: 'rap' }
+const map = new Map([['keyOne', 'aValue'], ['keyTwo', 'otherValue']])
+const _set = new Set([1, 2, 3, 4, 5])
 
 test(`returns true/false based on value's presence in collection`, t => {
   t.true(fn(str, 'r'))
@@ -15,6 +17,12 @@ test(`returns true/false based on value's presence in collection`, t => {
 
   t.true(fn(obj, 'rap'))
   t.false(fn(obj, 'folk'))
+
+  t.true(fn(map, 'otherValue'))
+  t.false(fn(map, 'notAValue'))
+
+  t.true(fn(_set, 2))
+  t.false(fn(_set, 6))
 })
 
 test('auto-curried at two arguments', t => {
