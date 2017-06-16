02d57d51e8787f986182bcd36af53bb64c1d0005
Bo Lingen <thecitycide@gmail.com>
2017-06-16 19:07:32 +0000

    style: change identifier name in test file

--- tests/random.js
+++ tests/random.js
@@ -1,35 +1,35 @@
 import test from 'ava'
-import toRandom from '../random'
+import fn from '../random'
 
 test('operates on array input', t => {
   let input = [1, 2, 3]
-  let result = toRandom(input)
+  let result = fn(input)
   t.true(input.indexOf(result) > -1)
 })
 
 test('operates on object input', t => {
   let values = [1, 2, 3]
   let input = { one: 1, two: 2, three: 3 }
-  let result = toRandom(input)
+  let result = fn(input)
   t.true(values.indexOf(result) > -1)
 })
 
 test('operates on number input, one argument', t => {
-  let result = toRandom(50)
+  let result = fn(50)
   t.true(typeof result === 'number')
   t.true(result >= 0 && result <= 50)
 
-  result = toRandom(1)
+  result = fn(1)
   t.true(typeof result === 'number')
   t.true(result >= 0 && result <= 1)
 })
 
 test('operates on number input, two arguments', t => {
-  let result = toRandom(-1, 4)
+  let result = fn(-1, 4)
   t.true(typeof result === 'number')
   t.true(result >= -1 && result <= 4)
 
-  result = toRandom(0, 0)
+  result = fn(0, 0)
   t.true(typeof result === 'number')
   t.true(result === 0)
 })
