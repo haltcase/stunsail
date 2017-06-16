e543167a5f67361fa9dfd320018997c80f28d8ae
Bo Lingen <thecitycide@gmail.com>
2017-06-16 18:27:49 +0000

    feat(is-empty): add support for more types

--- src/is-empty.js
+++ src/is-empty.js
@@ -1,29 +1,27 @@
 import getType from './get-type'
 import isArrayLike from './is-array-like'
-import isOneOf from './is-one-of'
-
-const INPUT_ARRAY_LIKES = [
-  'array',
-  'string',
-  'arguments',
-  'buffer'
-]
 
 export default value => {
   let type = getType(value)
+
+  switch (type) {
+    case 'null':
+    case 'undefined':
+      return true
+    case 'map':
+    case 'set':
+      return value.size === 0
+    case 'number': return value === 0
+    case 'object': return !Object.keys(value).length
+  }
+
   if (isArrayLike(value)) {
-    if (isOneOf(INPUT_ARRAY_LIKES, type)) {
-      return !value.length
-    }
+    return value.length === 0
   }
 
-  if (type === 'set' || type === 'map') {
-    return !value.size
+  if (/error/gi.test(type)) {
+    return value.message.length === 0
   }
 
-  if (type === 'object') {
-    return !Object.keys(value).length
-  }
-
-  return true
+  return !value
 }
