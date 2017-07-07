72a4571fe6b76f9a1cdba5008b96ffe37bd227c4
Bo Lingen <thecitycide@gmail.com>
2017-07-07 18:32:18 +0000

    feat: add `camelCase()`

---
+++ src/camel-case.js
@@ -0,0 +1,22 @@
+import {
+  WORD_SEPARATOR_REGEX as separators
+} from './constants'
+
+export default function camelCase (string) {
+  const words = string.split(separators)
+  const { length } = words
+  const result = new Array(length)
+
+  let i = -1
+  while (i++ < length) {
+    const word = words[i]
+    if (!word) continue
+
+    let [char] = word
+    char = i ? char.toUpperCase() : char.toLowerCase()
+    result[i] = char + word.slice(1)
+  }
+
+  return result.join('')
+}
+
---
+++ tests/camel-case.js
@@ -0,0 +1,11 @@
+import test from 'ava'
+import fn from '../camel-case'
+
+test('converts input strings to camel-case', t => {
+  t.is(fn('alreadyCamelCase'), 'alreadyCamelCase')
+  t.is(fn('Separated&By#$Characters'), 'separatedByCharacters')
+  t.is(fn('space separated string'), 'spaceSeparatedString')
+  t.is(fn('kebab-cased-string'), 'kebabCasedString')
+  t.is(fn('PascalCasedString'), 'pascalCasedString')
+  t.is(fn('snake_cased_string'), 'snakeCasedString')
+})
