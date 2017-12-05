9cea84b23dd21a9b1d185bf3b126ee09febd7fee
Bo Lingen <thecitycide@gmail.com>
2017-12-05 00:23:11 +0000

    docs: drop documentation generators [ci skip]

---
+++ script/create-index.js
@@ -0,0 +1,51 @@
+const { resolve } = require('path')
+const { readdirSync, writeFileSync } = require('fs')
+
+const src = resolve(__dirname, '..', 'src')
+const idx = resolve(src, 'index.js')
+
+const camelize = str =>
+  str.replace(/[-|.](\w)/g, (m, p1) => p1.toUpperCase())
+
+const getFiles = () =>
+  readdirSync(src).filter(file =>
+    (file.slice(-3) === '.js') && file !== 'index.js'
+  )
+
+const createIndex = () => {
+  const files = getFiles()
+
+  const [imports, outputs, defaults] =
+    files.reduce(([imports, outputs, defaults], file) => {
+      let filename = file.slice(0, -3)
+      const token = camelize(filename)
+
+      imports += token === 'constants'
+        ? `import * as ${token} from './${filename}'\n`
+        : `import { default as ${token} } from './${filename}'\n`
+
+      outputs += `export { ${token} }\n`
+      defaults += `  ${token},\n`
+
+      return [imports, outputs, defaults]
+    }, ['', '', 'export default {\n'])
+
+  const code =
+    imports + '\n' +
+    outputs + '\n' +
+    defaults.slice(0, -2) + '\n}\n'
+
+  writeFileSync(idx, code)
+
+  return files.length
+}
+
+try {
+  console.log('Creating index file...')
+  const quantity = createIndex()
+  console.log(`Index file created. (${quantity} source files)`)
+  process.exit(0)
+} catch (e) {
+  console.error(e)
+  process.exit(1)
+}
