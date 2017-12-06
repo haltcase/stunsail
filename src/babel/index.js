a444ac67e24266131d4052c081784436916e6455
citycide <thecitycide@gmail.com>
2017-12-06 01:56:52 -0600

    refactor: update babel plugin for let -> const, style

--- src/babel/index.js
+++ src/babel/index.js
@@ -1,13 +1,15 @@
 import kebab from '../kebab-case'
+import each from '../each'
 import filter from '../filter'
 
-let getNamespaces = filter(({ type }) => type === 'ImportSpecifier')
+const getNamespaces = list =>
+  filter(list, ({ type }) => type === 'ImportSpecifier')
 
-function makeImport (t, name, options) {
-  let { useRequire, useModules } = options
-  let file = kebab(name)
-  let es = !useRequire && useModules ? 'es/' : ''
-  let path = `stunsail/${es}${file}`
+const makeImport = (t, name, options) => {
+  const { useRequire, useModules } = options
+  const file = kebab(name)
+  const es = !useRequire && useModules ? 'es/' : ''
+  const path = `stunsail/${es}${file}`
 
   if (useRequire) {
     return t.variableDeclaration('const', [
@@ -27,7 +29,7 @@ function makeImport (t, name, options) {
   )
 }
 
-export default function ({ types: t }) {
+export default ({ types: t }) => {
   return {
     visitor: {
       CallExpression (path, state) {
@@ -36,14 +38,14 @@ export default function ({ types: t }) {
           !t.isStringLiteral(path.node.arguments[0], { value: 'stunsail' })
         ) return
 
-        let parent = path.parentPath.parentPath
-        let options = { useRequire: true, useModules: state.opts.useModules }
+        const parent = path.parentPath.parentPath
+        const options = { useRequire: true, useModules: state.opts.useModules }
 
-        let imports = []
-        parent.node.declarations.forEach(declaration => {
+        const imports = []
+        each(parent.node.declarations, declaration => {
           if (!t.isObjectPattern(declaration.id)) return
 
-          declaration.id.properties.forEach(property => {
+          each(declaration.id.properties, property => {
             imports.push(makeImport(t, property.key.name, options))
           })
         })
@@ -56,15 +58,15 @@ export default function ({ types: t }) {
       },
 
       ImportDeclaration (path, state) {
-        let { node: { source, specifiers } } = path
+        const { node: { source, specifiers } } = path
         if (source.value !== 'stunsail') return
 
-        let imports = []
-        let namespaces = getNamespaces(specifiers)
+        const imports = []
+        const namespaces = getNamespaces(specifiers)
 
-        namespaces.forEach(namespace => {
-          let { imported, local } = namespace
-          let name = imported.name || local.name
+        each(namespaces, namespace => {
+          const { imported, local } = namespace
+          const name = imported.name || local.name
           imports.push(makeImport(t, name, state.opts))
         })
 
