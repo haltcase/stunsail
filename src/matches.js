f5671620ce24a91b8e2936711392c9ffcc9087d6
citycide <thecitycide@gmail.com>
2017-12-09 11:34:00 -0600

    refactor(matches): shorten return / assignment

--- src/matches.js
+++ src/matches.js
@@ -5,8 +5,7 @@ export default function matches (object, compare) {
   let isMatch = true
   each(compare, (value, key) => {
     if (!isEqual(value, object[key])) {
-      isMatch = false
-      return false
+      return (isMatch = false)
     }
   })
 
