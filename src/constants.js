09916d8a2101b0b6956ec5475ea8c1b7675e91b6
Bo Lingen <thecitycide@gmail.com>
2019-01-15 19:20:15 +0000

    feat: add `textCase`

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,6 @@ export const BAD_HEX_REGEX = /^[-+]0x[0-9a-f]+$/i
 export const BINARY_REGEX = /^0b[01]+$/i
 export const WORD_SEPARATOR_REGEX = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-./:;<=>?@[\]^_`{|}~]+/
 export const UPPERCASE_CHARACTER_REGEX = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]/g
+export const LEFT_RIGHT_NONALPHANUM_REGEX = /^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g
+export const LOWER_UPPER_PAIR_REGEX = /([a-z])([A-Z])/g
+export const NONALPHANUM_OR_UNDERSCORE_REGEX = /[^A-Za-z0-9]+|_+/g
---
+++ src/text-case.js
@@ -0,0 +1,13 @@
+import {
+  LEFT_RIGHT_NONALPHANUM_REGEX,
+  LOWER_UPPER_PAIR_REGEX,
+  NONALPHANUM_OR_UNDERSCORE_REGEX
+} from './constants'
+
+export default function textCase (string) {
+  return string
+    .replace(LEFT_RIGHT_NONALPHANUM_REGEX, '')
+    .replace(LOWER_UPPER_PAIR_REGEX, (m, a, b) => a + '_' + b.toLowerCase())
+    .replace(NONALPHANUM_OR_UNDERSCORE_REGEX, ' ')
+    .toLowerCase()
+}
