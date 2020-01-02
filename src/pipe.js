6d317e2c9ff28cb41ad7869c69075fbf0b3ccc1d
citycide <lingenbw@gmail.com>
2020-01-01 18:36:29 -0600

    feat(pipe): change pipe to accept an argument list instead of array
    The argument list signature allows TypeScript to correctly infer types, while the array form doesn't (not sure why yet).
    
    BREAKING CHANGE: `pipe` now accepts multiple arguments rather than a single array.

--- src/pipe.js
+++ src/pipe.js
@@ -1,13 +1,6 @@
-import toArray from './to-array'
 import isThenable from './is-thenable'
 
 export default function pipe (...args) {
-  if (args.length > 1) {
-    throw new Error('`pipe` must be passed an array, not an argument list')
-  }
-
-  args = toArray(args[0])
-
   let output
   let i = -1
   const { length } = args
--- tests/pipe.js
+++ tests/pipe.js
@@ -2,11 +2,11 @@ import test from 'ava'
 import fn from '../prod/pipe'
 
 test('runs the given synchronous functions in series', async t => {
-  const result = await fn([
+  const result = await fn(
     'hello',
     str => str.toUpperCase(),
     str => str.split('').join('-')
-  ])
+  )
 
   t.is(result, 'H-E-L-L-O')
 })
@@ -16,10 +16,10 @@ test('runs the given asynchronous functions in series', async t => {
     return { name, favoriteColor: 'blue' }
   }
 
-  const result = await fn([
+  const result = await fn(
     name => getUserData(name),
     user => user.favoriteColor === 'blue'
-  ])
+  )
 
   t.true(result)
 })
